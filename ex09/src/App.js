89b99341e5fbe8b5bff2c2415d624081341d8148
Rusmira Sabic <sabic.rusmira@gmail.com>
2022-05-17 19:17:08 +0200

    Using props

---
+++ ex09/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Flower from './Flower';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Flower color='red'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ex10/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Box from './Box';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Box size='20'/>
+    </div>
+  );
+}
+
+export default App;
